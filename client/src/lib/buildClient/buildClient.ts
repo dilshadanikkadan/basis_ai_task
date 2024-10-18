76425ec977623d24d6b80c67a86e6a9ea1ccea0f
dilshadanikkadan <officialdilshad3312@gmail.com>
2024-10-18 06:57:37 +0530

    fix:cookie issue with vercle

--- client/src/lib/buildClient/buildClient.ts
+++ client/src/lib/buildClient/buildClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:3000",
-  baseURL: "https://basis-ai-task.onrender.com",
+  baseURL: "https://basis-ai-task-1.onrender.com",
   withCredentials: true,
 });
 
